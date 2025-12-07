8a5d160532414e719eb2ba3c525584c260764149
Nanami Nakagawa <ktc25a31e0010@edu.kyoto-tech.ac.jp>
2025-12-08 00:07:42 +0900

    ヒーローコンポーネントとスタイルの追加、メンバーコンポーネントの実装、404ページの作成

--- app/_components/Date/index.tsx
+++ app/_components/Date/index.tsx
@@ -3,7 +3,7 @@ import styles from "./index.module.css";
 
 type Props = {
     date: string;
-}:
+};
 
 export default function Date({ date }: Props) {
     return (
--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,19 +1,22 @@
 import Image from "next/image";
+import Link from "next/link"
 import styles from "./index.module.css";
-import { IBM_Plex_Sans_Devanagari } from "next/font/google";
+import Menu from "../Menu";
 
 export default function Header () {
     return (
         <header className={styles.headeraa}>
-            <a href="/" className={styles.logoLink}>
+            <Link href="/" className={styles.logoLink}>
                 <Image
                   src="logo.svg"
                   alt="SIMPLE"
                   className={styles.logo}
                   width={348}
                   height={133}
+                  priority
                 />
-            </a>
+            </Link>
+            <Menu />
         </header>
     );
 }
