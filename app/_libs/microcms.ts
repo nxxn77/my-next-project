554c26d63e7bdf37fb38fdd49251278a7afc5cf7
Nanami Nakagawa <ktc25a31e0010@edu.kyoto-tech.ac.jp>
2025-12-13 16:17:41 +0900

    ヘッダーのスタイル修正、ニュースリストコンポーネントの改善、定数ファイルの追加、MicroCMSクライアントの実装、メンバー情報の取得機能の追加、ページコンポーネントのリファクタリング、Next.jsの設定更新、依存関係の追加

--- app/_libs/microcms.ts
+++ app/_libs/microcms.ts
@@ -1,11 +1,57 @@
+import { createClient } from 'microcms-js-sdk';
+import type {
+    MicroCMSQueries,
+    MicroCMSImage,
+    MicroCMSListContent,
+    MicroCMSContentId,
+} from 'microcms-js-sdk';   
+
+export type member = {
+    name: string;
+    position: string;
+    plofile: string;
+    Image: string;
+} & MicroCMSContentId;
+
 export type Category = {
     name: string;
-};
+ } & MicroCMSListContent;
 
 export type News = {
-    id: string;
     title: string;
-    category: Category;
-    publishedAt: string;
-    createdAt: string;
+    description: string;
+    content: string;
+    thumbnail?: MicroCMSImage;
+    category?: Category;
+} & MicroCMSListContent;
+
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+    throw new Error("MICROCMS_SERVICE_DOMAIN is required");
+}
+
+if(!process.env.MICROCMS_API_KEY){
+    throw new Error("MICROCMS_API_KEY is required");
+}
+
+const client = createClient({
+    serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+    apiKey: process.env.MICROCMS_API_KEY,
+});
+
+export const getMembersList = async (queries?: MicroCMSQueries) => {
+    const listDate = await client
+    .getList<member>({
+        endpoint: "members",
+        queries,
+    });
+    return listDate;
+};
+
+export const getNewsList = async (queries?: MicroCMSQueries) => {
+    const listDate = await client
+    .getList<News>({
+        endpoint: "news",
+        queries,
+    });
+    return listDate;
 };
